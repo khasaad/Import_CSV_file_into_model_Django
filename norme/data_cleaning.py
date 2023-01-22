e50b02c9452dca13ed182e7809581fb846f122d6
khaled <khaledsaad.lb@gmail.com>
2023-01-22 03:46:16 +0200

    uploading csv file in model Django

---
+++ norme/data_cleaning.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+# Split file data.csv to several columns according to the symbol '|'
+def split_data(file_csv: str, cond: str):
+    dataframe = pd.read_csv(file_csv)
+    dataframe[list(dataframe.columns)[0].split(cond)] = dataframe[list(dataframe.columns)[0]].str.split(cond, expand=True)
+    dataframe.drop(columns=dataframe.columns[0], axis=1, inplace=True)
+    return dataframe
+
+if __name__ == '__main__':
+     df = split_data('data.csv', '|')
+     df.to_csv('new_data.csv', encoding='utf-8', index=False)
---
+++ norme/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+# Create your models here.
+class Norme (models.Model):
+    numdos = models.CharField(_("NUMDOS"), max_length=255)
+    numdosverling = models.CharField(_("NUMDOSVERLING"), max_length=255, blank=True)
+    ancart = models.CharField(_("ANCART"), max_length=255, blank=True)
+    filiere = models.CharField(_("FILIERE"), max_length=255, blank=True)
+    etape = models.FloatField()(_("ETAPE"), max_length=255, blank=True)
+    verling = models.TextField(_("VERLING"), max_length=255, blank=True)
+    formats = models.TextField(_("FORMAT"), max_length=255, blank=True)
+
+    def __str__(self):
+        return self.numdos
