80296129b5d5250df68feaecca951d037b48a897
khaled <khaledsaad.lb@gmail.com>
2023-02-07 15:03:20 +0200

    Updated

--- norme/views.py
+++ norme/views.py
@@ -1,8 +1,5 @@
 from django.http import JsonResponse
 from django.views import View
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-import json
 
 from .models import Norme
 
